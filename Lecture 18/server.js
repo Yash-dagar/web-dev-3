f626e0493acb8951bd076c95bea4075b9b02589b
yash <yashdagar2266@gmail.com>
2026-09-25 20:30:57 +0530

    modified:   Lecture 18/server.js

--- Lecture 18/server.js
+++ Lecture 18/server.js
@@ -2,7 +2,7 @@ const express=require("express");
 const app=express();
 const PORT=3000;
 const bodyParser=require("body-parser");
-comst path=require("path");
+const path=require("path");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
